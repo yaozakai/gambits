6b886caa06a86675e59a6f607f1fb94cd3780bf4
Walt Yao <walt.yao@gmail.com>
2023-04-14 21:19:10 +0800

    go reqs

--- go.py
+++ go.py
@@ -2,4 +2,5 @@ import sys
 import subprocess
 
 # implement pip as a subprocess:
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
+# subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
+subprocess.check_call([sys.executable, 'pip', 'install', '--upgrade', 'setuptools'])
