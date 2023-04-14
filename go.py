2747e730816b853d01408cb86baa983ea70b1a5d
Walt Yao <walt.yao@gmail.com>
2023-04-14 21:38:18 +0800

    go.py test

--- go.py
+++ go.py
@@ -2,6 +2,29 @@ import sys
 import subprocess
 
 # implement pip as a subprocess:
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip', 'setuptools', 'wheel'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip', 'setuptools', 'wheel',
+                       'captcha', 'click', 'Flask', 'Flask-Login'])
+# Flask
+#
+# Flask - Mail
+# flask_simple_captcha
+# flask_sqlalchemy
+# Flask - WTF
+# itsdangerous
+# Jinja2
+# MarkupSafe
+# psycopg2 - binary
+# pytz
+# requests
+# Werkzeug
+# WTForms
+# chinese_converter
+#
+# boto3
+#
+# conf
+#
+# SQLAlchemy
+
 # subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'])
 # subprocess.check_call([sys.executable, '-m',  'pip', 'install', '--upgrade', 'setuptools'])
