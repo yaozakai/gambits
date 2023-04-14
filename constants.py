94d1a4ef1abc638a10da6b863bcd7a79261675b7
Walt Yao <walt.yao@gmail.com>
2023-04-15 01:14:21 +0800

    sync

--- constants.py
+++ constants.py
@@ -4,5 +4,6 @@ RECAPTCHA_PUBLIC_KEY = "6LdaDkMkAAAAAEb-D-f0QwpY9VecOxeKNEyUzX-_"
 RECAPTCHA_PRIVATE_KEY = "6LdaDkMkAAAAADSxpkyC0xrSXRhrSceaU8GZ2F-O"
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:rR0sWw9GPJ14@gambits-db.cqzbnbnbi3rk.ap-northeast-2.rds.amazonaws.com:5432/postgres'
 # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://admin:2{C,ZGrm4f[S@185.145.131.100:3306/gambitsv_db'
+
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://admin:2{C,ZGrm4f[S@localhost:3306/gambitsv_db'
 AWS_SECRET_ACCESS_KEY = 'SFUB68Bagx/zFFX5H6KWZx7XtQCY//asIYei+UyU'
