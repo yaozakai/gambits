10df93ab78ac39bfe87e3dbc84a811e62e141990
Walt Yao <walt.yao@gmail.com>
2023-04-20 01:07:03 +0800

    host='0.0.0.0', app run

--- application.py
+++ application.py
@@ -431,7 +431,7 @@ if __name__ == '__main__':
     print('Socket: ' + socket.gethostname())
     # print('SQLALCHEMY_DATABASE_URI: ' + socket.gethostname())
     if socket.gethostname() == 'srv.gambits.vip':
-        application.run(port=80)
+        application.run(host='0.0.0.0', port=80)
     elif socket.gethostname() == 'The-Only-Real-MacBook-Pro.local':
         application.run()
 
