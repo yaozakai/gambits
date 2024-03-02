8b14bc4117641eed4d41d90c93871ceb961ea884
Walt <122274045+yaozakai@users.noreply.github.com>
2024-03-02 15:32:39 -0800

    setting up for GCP

--- appserver.py
+++ appserver.py
@@ -7,6 +7,6 @@ from os import environ
 
 create_app = create_app()
 if __name__ == '__main__':
-    # environ['env'] = 'prod'
-    create_app.run(host='192.168.1.107')  # for local testing (like with iPhone)
+    environ['env'] = 'prod'
+    create_app.run(port=5000, host='10.138.0.2')  # for local testing (like with iPhone)
     # create_app.run()  # for full integration with CQ9 (keep as localhost)
---
+++ start.sh
@@ -0,0 +1,3 @@
+cd /home/yaoza/gambits/
+source /home/yaoza/gambits/gambits/bin/activate
+python /home/yaoza/gambits/appserver.py
